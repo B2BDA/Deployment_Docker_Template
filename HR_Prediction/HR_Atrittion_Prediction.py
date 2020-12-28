4e0a7461a7693b33377733ec54b85b028070e05f
B2BDA <bishwarup1429@gmail.com>
2020-12-28 19:57:46 +0530

    added high accuracy classification

--- HR_Prediction/HR_Atrittion_Prediction.py
+++ HR_Prediction/HR_Atrittion_Prediction.py
@@ -3,57 +3,53 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import numpy as np
-model = pickle.load(open('HR_Atrittion.pkl','rb'))
+import pandas as pd
+model = pickle.load(open('HR_Atrittion1.pkl','rb'))
+encoder = pickle.load(open('encoder.pkl','rb'))
+
 app = FastAPI()
 
 class DataIn(BaseModel):
-    Age : int
-    DistanceFromHome : int
-    EnvironmentSatisfaction :  int
-    JobInvolvement : int
-    JobLevel : int
-    MonthlyIncome : int
-    NumCompaniesWorked : int
-    PercentSalaryHike : int
-    PerformanceRating : int
-    RelationshipSatisfaction : int
-    StandardHours : int
-    StockOptionLevel : int
-    TotalWorkingYears : int
-    TrainingTimesLastYear : int
-    WorkLifeBalance : int
-    YearsAtCompany : int
-    YearsInCurrentRole : int
-    YearsSinceLastPromotion : int
-    YearsWithCurrManager : int
-    JobSatisfaction : int
-    BusinessTravel_Travel_Frequently : int
-    BusinessTravel_Travel_Rarely : int
-    Department_Research_Development : int
-    Department_Sales : int
-    Gender_Male : int
-    JobRole_Human_Resources : int 
-    JobRole_Laboratory_Technician : int
-    JobRole_Manager :  int
-    JobRole_Manufacturing_Director : int
-    JobRole_Research_Director : int
-    JobRole_Research_Scientist : int
-    JobRole_Sales_Executive : int
-    JobRole_Sales_Representative : int
-    MaritalStatus_Married : int
-    MaritalStatus_Single : int
-    OverTime_Yes : int
+    
+    Age: int
+    DistanceFromHome: int
+    EnvironmentSatisfaction: int
+    JobInvolvement: int
+    JobLevel: int
+    JobSatisfaction: int
+    MonthlyIncome: int
+    NumCompaniesWorked: int
+    PercentSalaryHike: int
+    PerformanceRating: int
+    RelationshipSatisfaction: int
+    StandardHours: int
+    StockOptionLevel: int
+    TotalWorkingYears: int
+    TrainingTimesLastYear: int
+    WorkLifeBalance: int
+    YearsAtCompany: int
+    YearsInCurrentRole: int
+    YearsSinceLastPromotion: int
+    YearsWithCurrManager: int
+    BusinessTravel: str
+    Department: str
+    Gender: str
+    JobRole: str
+    MaritalStatus: str
+    OverTime: str
     
 @app.get('/')
-def greetings():
+async def greetings():
     return {"greeting":"Hello User"}
 
 @app.post('/predict/')
 async def create_user(dataIn : DataIn):
     data = dict(dataIn).values()
-    data = np.array(list(data))
-    prediction = model.predict(data.reshape(-1,36))
-    return 'The predicted value is '+ str(prediction[0])
+    data1 = np.array(list(data)[0:20]).reshape(1,-1)
+    data2 = encoder.transform(np.array(list(data)[20:]).reshape(1,-1))
+    data =  np.hstack((data1, data2))
+    prediction = model.predict_proba(data)
+    return 'There is a probability of '+ str(prediction[0][1]) + '  the employee leaving the org'
 
 if __name__ == '__main__':
     uvicorn.run(app,host='localhost',port=8000)
